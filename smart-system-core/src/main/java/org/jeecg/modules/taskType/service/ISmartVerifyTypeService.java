91b60eeee7a4f25c52ad0448ba52ef9237547f9d
sikex <yoloxz719@gmail.com>
2021-11-05 14:18:44 +0800

    fix confilx

---
+++ smart-system-core/src/main/java/org/jeecg/modules/taskType/service/ISmartVerifyTypeService.java
@@ -0,0 +1,14 @@
+package org.jeecg.modules.taskType.service;
+
+import org.jeecg.modules.taskType.entity.SmartVerifyType;
+import com.baomidou.mybatisplus.extension.service.IService;
+
+/**
+ * @Description: 审核任务类型编码表
+ * @Author: jeecg-boot
+ * @Date:   2021-11-04
+ * @Version: V1.0
+ */
+public interface ISmartVerifyTypeService extends IService<SmartVerifyType> {
+
+}
---
+++ smart-system-core/src/main/java/org/jeecg/modules/tasks/service/impl/SmartVerifyTaskServiceImpl.java
@@ -0,0 +1,19 @@
+package org.jeecg.modules.tasks.service.impl;
+
+import org.jeecg.modules.tasks.entity.SmartVerifyTask;
+import org.jeecg.modules.tasks.mapper.SmartVerifyTaskMapper;
+import org.jeecg.modules.tasks.service.ISmartVerifyTaskService;
+import org.springframework.stereotype.Service;
+
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+
+/**
+ * @Description: 审核任务表
+ * @Author: jeecg-boot
+ * @Date:   2021-11-04
+ * @Version: V1.0
+ */
+@Service
+public class SmartVerifyTaskServiceImpl extends ServiceImpl<SmartVerifyTaskMapper, SmartVerifyTask> implements ISmartVerifyTaskService {
+
+}
