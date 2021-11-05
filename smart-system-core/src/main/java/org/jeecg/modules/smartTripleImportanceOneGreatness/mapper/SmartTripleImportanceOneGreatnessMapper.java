98764d9f835bace0048373651bd4eff85958c998
WangQingyuwqy <1369272088@qq.com>
2021-11-05 09:35:02 +0800

    表后端

--- smart-system-core/src/main/java/org/jeecg/modules/smartTripleImportanceOneGreatness/mapper/SmartTripleImportanceOneGreatnessMapper.java
+++ smart-system-core/src/main/java/org/jeecg/modules/smartTripleImportanceOneGreatness/mapper/SmartTripleImportanceOneGreatnessMapper.java
@@ -9,7 +9,7 @@ import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 /**
  * @Description: 三重一大表
  * @Author: jeecg-boot
- * @Date:   2021-11-04
+ * @Date:   2021-11-05
  * @Version: V1.0
  */
 public interface SmartTripleImportanceOneGreatnessMapper extends BaseMapper<SmartTripleImportanceOneGreatness> {
