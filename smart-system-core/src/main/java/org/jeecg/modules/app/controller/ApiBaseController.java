9f6bb4aa70879e809a5886691272502218fafeaa
CabbSir <cabbsir@gmail.com>
2021-11-06 20:01:26 +0800

    提交接口基类，校验sign的方法

--- smart-system-core/src/main/java/org/jeecg/modules/app/controller/ApiBaseController.java
+++ smart-system-core/src/main/java/org/jeecg/modules/app/controller/ApiBaseController.java
@@ -1,13 +1,20 @@
 package org.jeecg.modules.app.controller;
 
+import org.jeecg.modules.app.util.Encrypt;
+
 import java.util.Map;
+import java.util.TreeMap;
 
 public class ApiBaseController {
-    protected Boolean checkSign(Map<String, Object> params) {
-        for (String key : params.keySet()) {
-            System.out.print("Key = " + key + " ");
-            System.out.println("Value = " + params.get(key));
+    protected Boolean checkSign(Map<String, String> params) {
+        TreeMap<String, String> orderedParams = new TreeMap<>(params);
+        String sign = orderedParams.get("sign");
+        orderedParams.remove("sign");
+        StringBuilder mySign = new StringBuilder();
+        for (String key : orderedParams.keySet()) {
+            mySign.append(key).append("=").append(orderedParams.get(key));
         }
-        return true;
+        return Encrypt.MD5("6clnOgAvp5HulYNTOma5" +
+                Encrypt.SHA256(mySign + "6clnOgAvp5HulYNTOma5")).equals(sign);
     }
 }
