794906f36a1f0ef6b03e6073a961b18830681406
xyy <machine.xyy@gmail.com>
2021-11-11 11:54:28 +0800

    组织生活会和财物收支添加权限控制2.0，可使用高级查询。并将查询结果的单位ID变成名称，添加公共模块common

---
+++ smart-system-core/src/main/java/org/jeecg/modules/smartFinanceResult/controller/SmartFinanceResultController.java
@@ -0,0 +1,321 @@
+package org.jeecg.modules.smartFinanceResult.controller;
+
+import java.io.UnsupportedEncodingException;
+import java.io.IOException;
+import java.net.URLDecoder;
+import java.util.*;
+import java.util.stream.Collectors;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.jeecg.modules.common.service.CommonService;
+import org.jeecg.modules.common.util.ParamsUtil;
+import org.jeecg.modules.smartOrgMeeting.entity.SmartOrgMeeting;
+import org.jeecgframework.poi.excel.ExcelImportUtil;
+import org.jeecgframework.poi.excel.def.NormalExcelConstants;
+import org.jeecgframework.poi.excel.entity.ExportParams;
+import org.jeecgframework.poi.excel.entity.ImportParams;
+import org.jeecgframework.poi.excel.view.JeecgEntityExcelView;
+import org.jeecg.common.system.vo.LoginUser;
+import org.apache.shiro.SecurityUtils;
+import org.jeecg.common.api.vo.Result;
+import org.jeecg.common.system.query.QueryGenerator;
+import org.jeecg.common.util.oConvertUtils;
+import org.jeecg.modules.smartFinanceResult.entity.SmartFinanceAnnex;
+import org.jeecg.modules.smartFinanceResult.entity.SmartFinanceResult;
+import org.jeecg.modules.smartFinanceResult.vo.SmartFinanceResultPage;
+import org.jeecg.modules.smartFinanceResult.service.ISmartFinanceResultService;
+import org.jeecg.modules.smartFinanceResult.service.ISmartFinanceAnnexService;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.multipart.MultipartHttpServletRequest;
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.baomidou.mybatisplus.core.metadata.IPage;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import lombok.extern.slf4j.Slf4j;
+import com.alibaba.fastjson.JSON;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import org.jeecg.common.aspect.annotation.AutoLog;
+
+/**
+ * @Description: 8项规定财物收支表
+ * @Author: jeecg-boot
+ * @Date: 2021-11-10
+ * @Version: V1.0
+ */
+@Api(tags = "8项规定财物收支表")
+@RestController
+@RequestMapping("/smartFinanceResult/smartFinanceResult")
+@Slf4j
+public class SmartFinanceResultController {
+    @Autowired
+    private ISmartFinanceResultService smartFinanceResultService;
+    @Autowired
+    private ISmartFinanceAnnexService smartFinanceAnnexService;
+    @Autowired
+    CommonService commonService;
+
+    /**
+     * 分页列表查询
+     *
+     * @param smartFinanceResult
+     * @param pageNo
+     * @param pageSize
+     * @param req
+     * @return
+     */
+    @AutoLog(value = "8项规定财物收支表-分页列表查询")
+    @ApiOperation(value = "8项规定财物收支表-分页列表查询", notes = "8项规定财物收支表-分页列表查询")
+    @GetMapping(value = "/list")
+    public Result<?> queryPageList(SmartFinanceResult smartFinanceResult,
+                                   @RequestParam(name = "pageNo", defaultValue = "1") Integer pageNo,
+                                   @RequestParam(name = "pageSize", defaultValue = "10") Integer pageSize,
+                                   HttpServletRequest req) {
+        // 1. 规则，下面是 以**开始
+        String rule = "in";
+        // 2. 查询字段
+        String field = "departId";
+        // 获取登录用户信息，可以用来查询单位部门信息
+        LoginUser sysUser = (LoginUser) SecurityUtils.getSubject().getPrincipal();
+
+        // 获取子单位ID
+        String childrenIdString = commonService.getChildrenIdStringByOrgCode(sysUser.getOrgCode());
+
+        HashMap<String, String[]> map = new HashMap<>(req.getParameterMap());
+        // 获取请求参数中的superQueryParams
+        List<String> paramsList = ParamsUtil.getSuperQueryParams(req.getParameterMap());
+
+        // 添加额外查询条件，用于权限控制
+        paramsList.add("%5B%7B%22rule%22:%22" + rule + "%22,%22type%22:%22string%22,%22dictCode%22:%22%22,%22val%22:%22"
+                + childrenIdString
+                + "%22,%22field%22:%22" + field + "%22%7D%5D");
+        String[] params = new String[paramsList.size()];
+        paramsList.toArray(params);
+        map.put("superQueryParams", params);
+        params = new String[]{"and"};
+        map.put("superQueryMatchType", params);
+
+
+        QueryWrapper<SmartFinanceResult> queryWrapper = QueryGenerator.initQueryWrapper(smartFinanceResult, map);
+        Page<SmartFinanceResult> page = new Page<SmartFinanceResult>(pageNo, pageSize);
+        IPage<SmartFinanceResult> pageList = smartFinanceResultService.page(page, queryWrapper);
+        List<String> departIds = pageList.getRecords().stream().map(SmartFinanceResult::getDepartId).collect(Collectors.toList());
+        if (departIds != null && departIds.size() > 0) {
+            Map<String, String> useDepNames = commonService.getDepNamesByIds(departIds);
+            pageList.getRecords().forEach(item -> {
+                item.setDepartId(useDepNames.get(item.getDepartId()));
+            });
+        }
+        return Result.OK(pageList);
+    }
+
+    /**
+     * 添加
+     *
+     * @param smartFinanceResultPage
+     * @return
+     */
+    @AutoLog(value = "8项规定财物收支表-添加")
+    @ApiOperation(value = "8项规定财物收支表-添加", notes = "8项规定财物收支表-添加")
+    @PostMapping(value = "/add")
+    public Result<?> add(@RequestBody SmartFinanceResultPage smartFinanceResultPage) {
+        SmartFinanceResult smartFinanceResult = new SmartFinanceResult();
+        BeanUtils.copyProperties(smartFinanceResultPage, smartFinanceResult);
+        LoginUser sysUser = (LoginUser) SecurityUtils.getSubject().getPrincipal();
+        String orgCode = sysUser.getOrgCode();
+        if ("".equals(orgCode)) {
+            return Result.error("本用户没有操作权限！");
+        }
+        String id = commonService.getDepartIdByOrgCode(orgCode);
+        if (id == null) {
+            return Result.error("没有找到部门！");
+        }
+        smartFinanceResult.setDepartId(id);
+        smartFinanceResultService.saveMain(smartFinanceResult, smartFinanceResultPage.getSmartFinanceAnnexList());
+        return Result.OK("添加成功！");
+    }
+
+    /**
+     * 编辑
+     *
+     * @param smartFinanceResultPage
+     * @return
+     */
+    @AutoLog(value = "8项规定财物收支表-编辑")
+    @ApiOperation(value = "8项规定财物收支表-编辑", notes = "8项规定财物收支表-编辑")
+    @PutMapping(value = "/edit")
+    public Result<?> edit(@RequestBody SmartFinanceResultPage smartFinanceResultPage) {
+        SmartFinanceResult smartFinanceResult = new SmartFinanceResult();
+        BeanUtils.copyProperties(smartFinanceResultPage, smartFinanceResult);
+        SmartFinanceResult smartFinanceResultEntity = smartFinanceResultService.getById(smartFinanceResult.getId());
+        if (smartFinanceResultEntity == null) {
+            return Result.error("未找到对应数据");
+        }
+        smartFinanceResult.setDepartId(null);
+        smartFinanceResult.setCreateTime(null);
+        smartFinanceResultService.updateMain(smartFinanceResult, smartFinanceResultPage.getSmartFinanceAnnexList());
+        return Result.OK("编辑成功!");
+    }
+
+    /**
+     * 通过id删除
+     *
+     * @param id
+     * @return
+     */
+    @AutoLog(value = "8项规定财物收支表-通过id删除")
+    @ApiOperation(value = "8项规定财物收支表-通过id删除", notes = "8项规定财物收支表-通过id删除")
+    @DeleteMapping(value = "/delete")
+    public Result<?> delete(@RequestParam(name = "id", required = true) String id) {
+        smartFinanceResultService.delMain(id);
+        return Result.OK("删除成功!");
+    }
+
+    /**
+     * 批量删除
+     *
+     * @param ids
+     * @return
+     */
+    @AutoLog(value = "8项规定财物收支表-批量删除")
+    @ApiOperation(value = "8项规定财物收支表-批量删除", notes = "8项规定财物收支表-批量删除")
+    @DeleteMapping(value = "/deleteBatch")
+    public Result<?> deleteBatch(@RequestParam(name = "ids", required = true) String ids) {
+        this.smartFinanceResultService.delBatchMain(Arrays.asList(ids.split(",")));
+        return Result.OK("批量删除成功！");
+    }
+
+    /**
+     * 通过id查询
+     *
+     * @param id
+     * @return
+     */
+    @AutoLog(value = "8项规定财物收支表-通过id查询")
+    @ApiOperation(value = "8项规定财物收支表-通过id查询", notes = "8项规定财物收支表-通过id查询")
+    @GetMapping(value = "/queryById")
+    public Result<?> queryById(@RequestParam(name = "id", required = true) String id) {
+        SmartFinanceResult smartFinanceResult = smartFinanceResultService.getById(id);
+        if (smartFinanceResult == null) {
+            return Result.error("未找到对应数据");
+        }
+        return Result.OK(smartFinanceResult);
+
+    }
+
+    /**
+     * 通过id查询
+     *
+     * @param id
+     * @return
+     */
+    @AutoLog(value = "8项规定财物收支附件通过主表ID查询")
+    @ApiOperation(value = "8项规定财物收支附件主表ID查询", notes = "8项规定财物收支附件-通主表ID查询")
+    @GetMapping(value = "/querySmartFinanceAnnexByMainId")
+    public Result<?> querySmartFinanceAnnexListByMainId(@RequestParam(name = "id", required = true) String id) {
+        List<SmartFinanceAnnex> smartFinanceAnnexList = smartFinanceAnnexService.selectByMainId(id);
+        return Result.OK(smartFinanceAnnexList);
+    }
+
+    /**
+     * 导出excel
+     *
+     * @param request
+     * @param smartFinanceResult
+     */
+    @RequestMapping(value = "/exportXls")
+    public ModelAndView exportXls(HttpServletRequest request, SmartFinanceResult smartFinanceResult) {
+        // Step.1 组装查询条件查询数据
+        QueryWrapper<SmartFinanceResult> queryWrapper = QueryGenerator.initQueryWrapper(smartFinanceResult, request.getParameterMap());
+        LoginUser sysUser = (LoginUser) SecurityUtils.getSubject().getPrincipal();
+
+        //Step.2 获取导出数据
+        List<SmartFinanceResult> queryList = smartFinanceResultService.list(queryWrapper);
+        // 过滤选中数据
+        String selections = request.getParameter("selections");
+        List<SmartFinanceResult> smartFinanceResultList = new ArrayList<SmartFinanceResult>();
+        if (oConvertUtils.isEmpty(selections)) {
+            smartFinanceResultList = queryList;
+        } else {
+            List<String> selectionList = Arrays.asList(selections.split(","));
+            smartFinanceResultList = queryList.stream().filter(item -> selectionList.contains(item.getId())).collect(Collectors.toList());
+        }
+
+        // Step.3 组装pageList
+        List<SmartFinanceResultPage> pageList = new ArrayList<SmartFinanceResultPage>();
+        for (SmartFinanceResult main : smartFinanceResultList) {
+            SmartFinanceResultPage vo = new SmartFinanceResultPage();
+            BeanUtils.copyProperties(main, vo);
+            List<SmartFinanceAnnex> smartFinanceAnnexList = smartFinanceAnnexService.selectByMainId(main.getId());
+            vo.setSmartFinanceAnnexList(smartFinanceAnnexList);
+            pageList.add(vo);
+        }
+
+        // Step.4 AutoPoi 导出Excel
+        ModelAndView mv = new ModelAndView(new JeecgEntityExcelView());
+        mv.addObject(NormalExcelConstants.FILE_NAME, "8项规定财物收支表列表");
+        mv.addObject(NormalExcelConstants.CLASS, SmartFinanceResultPage.class);
+        mv.addObject(NormalExcelConstants.PARAMS, new ExportParams("8项规定财物收支表数据", "导出人:" + sysUser.getRealname(), "8项规定财物收支表"));
+        mv.addObject(NormalExcelConstants.DATA_LIST, pageList);
+        return mv;
+    }
+
+    /**
+     * 通过excel导入数据
+     *
+     * @param request
+     * @param response
+     * @return
+     */
+    @RequestMapping(value = "/importExcel", method = RequestMethod.POST)
+    public Result<?> importExcel(HttpServletRequest request, HttpServletResponse response) {
+        MultipartHttpServletRequest multipartRequest = (MultipartHttpServletRequest) request;
+        Map<String, MultipartFile> fileMap = multipartRequest.getFileMap();
+        for (Map.Entry<String, MultipartFile> entity : fileMap.entrySet()) {
+            MultipartFile file = entity.getValue();// 获取上传文件对象
+            ImportParams params = new ImportParams();
+            params.setTitleRows(2);
+            params.setHeadRows(1);
+            params.setNeedSave(true);
+            try {
+                List<SmartFinanceResultPage> list = ExcelImportUtil.importExcel(file.getInputStream(), SmartFinanceResultPage.class, params);
+                for (SmartFinanceResultPage page : list) {
+                    SmartFinanceResult po = new SmartFinanceResult();
+                    BeanUtils.copyProperties(page, po);
+                    smartFinanceResultService.saveMain(po, page.getSmartFinanceAnnexList());
+                }
+                return Result.OK("文件导入成功！数据行数:" + list.size());
+            } catch (Exception e) {
+                log.error(e.getMessage(), e);
+                return Result.error("文件导入失败:" + e.getMessage());
+            } finally {
+                try {
+                    file.getInputStream().close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+        return Result.OK("文件导入失败！");
+    }
+
+    @AutoLog(value = "更新文件下载次数")
+    @ApiOperation(value = "更新文件下载次数", notes = "更新文件下载次数")
+    @PutMapping(value = "/downloadCount")
+    public Result<?> downloadCount(@RequestBody SmartFinanceAnnex smartFinanceAnnex) {
+        SmartFinanceAnnex newSmartFinanceAnnex = smartFinanceAnnexService.getById(smartFinanceAnnex.getId());
+        if (newSmartFinanceAnnex == null) {
+            return Result.error("未找到对应数据");
+        }
+        Integer downloadCount = newSmartFinanceAnnex.getDownloadCount();
+        newSmartFinanceAnnex.setDownloadCount(downloadCount + 1);
+        smartFinanceAnnexService.updateById(newSmartFinanceAnnex);
+        return Result.OK("更新成功!");
+    }
+
+}
---
+++ smart-system-core/src/main/java/org/jeecg/modules/smartOrgMeeting/service/impl/SmartOrgMeetingServiceImpl.java
@@ -0,0 +1,98 @@
+package org.jeecg.modules.smartOrgMeeting.service.impl;
+
+import org.jeecg.modules.smartOrgMeeting.entity.SmartOrgMeeting;
+import org.jeecg.modules.smartOrgMeeting.entity.SmartOrgMeetingPacpa;
+import org.jeecg.modules.smartOrgMeeting.entity.SmartOrgMeetingAnnex;
+import org.jeecg.modules.smartOrgMeeting.mapper.SmartOrgMeetingPacpaMapper;
+import org.jeecg.modules.smartOrgMeeting.mapper.SmartOrgMeetingAnnexMapper;
+import org.jeecg.modules.smartOrgMeeting.mapper.SmartOrgMeetingMapper;
+import org.jeecg.modules.smartOrgMeeting.service.ISmartOrgMeetingService;
+import org.springframework.stereotype.Service;
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.transaction.annotation.Transactional;
+import java.io.Serializable;
+import java.util.List;
+import java.util.Collection;
+
+/**
+ * @Description: 组织生活会
+ * @Author: jeecg-boot
+ * @Date:   2021-11-11
+ * @Version: V1.0
+ */
+@Service
+public class SmartOrgMeetingServiceImpl extends ServiceImpl<SmartOrgMeetingMapper, SmartOrgMeeting> implements ISmartOrgMeetingService {
+
+	@Autowired
+	private SmartOrgMeetingMapper smartOrgMeetingMapper;
+	@Autowired
+	private SmartOrgMeetingPacpaMapper smartOrgMeetingPacpaMapper;
+	@Autowired
+	private SmartOrgMeetingAnnexMapper smartOrgMeetingAnnexMapper;
+	
+	@Override
+	@Transactional
+	public void saveMain(SmartOrgMeeting smartOrgMeeting, List<SmartOrgMeetingPacpa> smartOrgMeetingPacpaList,List<SmartOrgMeetingAnnex> smartOrgMeetingAnnexList) {
+		smartOrgMeetingMapper.insert(smartOrgMeeting);
+		if(smartOrgMeetingPacpaList!=null && smartOrgMeetingPacpaList.size()>0) {
+			for(SmartOrgMeetingPacpa entity:smartOrgMeetingPacpaList) {
+				//外键设置
+				entity.setParentId(smartOrgMeeting.getId());
+				smartOrgMeetingPacpaMapper.insert(entity);
+			}
+		}
+		if(smartOrgMeetingAnnexList!=null && smartOrgMeetingAnnexList.size()>0) {
+			for(SmartOrgMeetingAnnex entity:smartOrgMeetingAnnexList) {
+				//外键设置
+				entity.setParentId(smartOrgMeeting.getId());
+				smartOrgMeetingAnnexMapper.insert(entity);
+			}
+		}
+	}
+
+	@Override
+	@Transactional
+	public void updateMain(SmartOrgMeeting smartOrgMeeting,List<SmartOrgMeetingPacpa> smartOrgMeetingPacpaList,List<SmartOrgMeetingAnnex> smartOrgMeetingAnnexList) {
+		smartOrgMeetingMapper.updateById(smartOrgMeeting);
+		
+		//1.先删除子表数据
+		smartOrgMeetingPacpaMapper.deleteByMainId(smartOrgMeeting.getId());
+		smartOrgMeetingAnnexMapper.deleteByMainId(smartOrgMeeting.getId());
+		
+		//2.子表数据重新插入
+		if(smartOrgMeetingPacpaList!=null && smartOrgMeetingPacpaList.size()>0) {
+			for(SmartOrgMeetingPacpa entity:smartOrgMeetingPacpaList) {
+				//外键设置
+				entity.setParentId(smartOrgMeeting.getId());
+				smartOrgMeetingPacpaMapper.insert(entity);
+			}
+		}
+		if(smartOrgMeetingAnnexList!=null && smartOrgMeetingAnnexList.size()>0) {
+			for(SmartOrgMeetingAnnex entity:smartOrgMeetingAnnexList) {
+				//外键设置
+				entity.setParentId(smartOrgMeeting.getId());
+				smartOrgMeetingAnnexMapper.insert(entity);
+			}
+		}
+	}
+
+	@Override
+	@Transactional
+	public void delMain(String id) {
+		smartOrgMeetingPacpaMapper.deleteByMainId(id);
+		smartOrgMeetingAnnexMapper.deleteByMainId(id);
+		smartOrgMeetingMapper.deleteById(id);
+	}
+
+	@Override
+	@Transactional
+	public void delBatchMain(Collection<? extends Serializable> idList) {
+		for(Serializable id:idList) {
+			smartOrgMeetingPacpaMapper.deleteByMainId(id.toString());
+			smartOrgMeetingAnnexMapper.deleteByMainId(id.toString());
+			smartOrgMeetingMapper.deleteById(id);
+		}
+	}
+	
+}
