baebcf7525c72592fc9014d423fe1d9ba78db652
WangQingyuwqy <1369272088@qq.com>
2021-11-12 14:11:29 +0800

    纠治四风后端+审核+逻辑删除

--- smart-system-core/src/main/java/org/jeecg/modules/smartReportingInformation/service/ISmartReportingInformationService.java
+++ smart-system-core/src/main/java/org/jeecg/modules/smartReportingInformation/service/ISmartReportingInformationService.java
@@ -6,7 +6,7 @@ import com.baomidou.mybatisplus.extension.service.IService;
 /**
  * @Description: 举报信息表
  * @Author: jeecg-boot
- * @Date:   2021-11-07
+ * @Date:   2021-11-12
  * @Version: V1.0
  */
 public interface ISmartReportingInformationService extends IService<SmartReportingInformation> {
