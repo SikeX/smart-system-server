a50157ca25949ee983adcfe7d0a3b5a138e757b5
CabbSir <cabbsir@gmail.com>
2021-11-15 20:49:23 +0800

    配合完善app接口

--- jeecg-boot-module-system/src/main/java/org/jeecg/modules/app/mapper/AppUserMapper.java
+++ jeecg-boot-module-system/src/main/java/org/jeecg/modules/app/mapper/AppUserMapper.java
@@ -22,11 +22,11 @@ public interface AppUserMapper {
     AppUser queryByAndroidId(String androidId);
 
     @Update("UPDATE tb_app_user SET last_login_ip = #{lastLoginIp}, mac = #{mac}, app_version = #{appVersion}, " +
-            "brand = #{brand}, phone_model = #{phoneModel}, last_login_time = #{lastLoginTime}, mtime = #{mtime} " +
-            "where id = #{id}")
+            "brand = #{brand}, phone_model = #{phoneModel}, last_login_time = #{lastLoginTime}, mtime = #{mtime}, " +
+            "sys_user_id = #{sysUserId} where id = #{id}")
     int updateById(AppUser appUser);
 
     @Update("UPDATE tb_app_user SET sys_user_id = #{sysUserId}, mtime = #{mtime} " +
             "where id = #{id}")
-    int updateSysUserIdById(int id, String sysUserId);
+    int updateSysUserIdById(int id, String sysUserId, int mtime);
 }
