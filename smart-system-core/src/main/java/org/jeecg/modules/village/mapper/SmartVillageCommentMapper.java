8c7fe82598754d1c772d32fd3c438dbe52bf6b0c
MrYsL <1501010883@qq.com>
2021-11-23 21:20:54 +0800

    村情互动首页

---
+++ smart-system-core/src/main/java/org/jeecg/modules/village/mapper/SmartVillageCommentMapper.java
@@ -0,0 +1,14 @@
+package org.jeecg.modules.village.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import org.jeecg.modules.village.entity.SmartVillageComment;
+
+/**
+ * @Description: 村情互动-回复表
+ * @Author: jeecg-boot
+ * @Date:   2021-11-22
+ * @Version: V1.0
+ */
+public interface SmartVillageCommentMapper extends BaseMapper<SmartVillageComment> {
+
+}
---
+++ smart-system-core/src/main/java/org/jeecg/modules/village/service/ISmartVillageCommentService.java
@@ -0,0 +1,14 @@
+package org.jeecg.modules.village.service;
+
+import com.baomidou.mybatisplus.extension.service.IService;
+import org.jeecg.modules.village.entity.SmartVillageComment;
+
+/**
+ * @Description: 村情互动-回复表
+ * @Author: jeecg-boot
+ * @Date:   2021-11-22
+ * @Version: V1.0
+ */
+public interface ISmartVillageCommentService extends IService<SmartVillageComment> {
+
+}
