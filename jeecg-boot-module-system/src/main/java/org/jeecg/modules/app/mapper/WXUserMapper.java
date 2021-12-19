8ba1b86f156683c6177b30adb2e5281deaf2449b
CabbSir <cabbsir@gmail.com>
2021-12-19 12:29:45 +0800

    加入Param注解解决线上bug

--- jeecg-boot-module-system/src/main/java/org/jeecg/modules/app/mapper/WXUserMapper.java
+++ jeecg-boot-module-system/src/main/java/org/jeecg/modules/app/mapper/WXUserMapper.java
@@ -1,9 +1,6 @@
 package org.jeecg.modules.app.mapper;
 
-import org.apache.ibatis.annotations.Insert;
-import org.apache.ibatis.annotations.Mapper;
-import org.apache.ibatis.annotations.Select;
-import org.apache.ibatis.annotations.Update;
+import org.apache.ibatis.annotations.*;
 import org.jeecg.modules.app.entity.AppUser;
 import org.jeecg.modules.app.entity.WXUser;
 
@@ -28,14 +25,14 @@ public interface WXUserMapper {
 
     @Update("UPDATE tb_wx_user SET sys_user_id = #{sysUserId}, mtime = #{mtime} " +
             "where id = #{id}")
-    void updateSysUserIdById(int id, String sysUserId, int mtime);
+    void updateSysUserIdById(@Param("id") int id, @Param("sysUserId") String sysUserId, @Param("mtime") int mtime);
 
     @Select("SELECT * FROM tb_wx_user WHERE wx_session_key = #{sessionKey}")
     WXUser queryBySessionKey(String sessionKey);
 
     @Update("UPDATE tb_wx_user SET phone = #{purePhoneNumber} where id = #{id}")
-    void updatePhoneById(int id, String purePhoneNumber);
+    void updatePhoneById(@Param("id") int id, @Param("purePhoneNumber") String purePhoneNumber);
 
     @Update("UPDATE tb_wx_user SET sys_user_id = #{sysUserId} where id = #{id}")
-    void updateWxUserSysUserIdById(int id, String sysUserId);
+    void updateWxUserSysUserIdById(@Param("id") int id, @Param("sysUserId") String sysUserId);
 }
