db723dcd40314caab6bf9d40006d4a00829fe714
MrYsL <51028042+mrysl@users.noreply.github.com>
2022-02-26 19:21:39 +0800

    户籍导入，户籍管理后台修改 (#126)
    * 部门编辑涉及到相关用户orgCode更改
    
    * 纪委管理员村民管理，户籍创建
    
    * 纪委管理员村民管理，户籍创建
    
    * 修复bug
    
    * 户主电话号不可为空
    
    * 超权限人员管理编辑人员
    
    * 解决编辑无账号村民时报错的问题
    
    * 八项规定首页
    
    * 户籍导入+户籍管理后台修改

--- jeecg-boot-module-system/src/main/java/org/jeecg/modules/system/entity/SysUser.java
+++ jeecg-boot-module-system/src/main/java/org/jeecg/modules/system/entity/SysUser.java
@@ -105,8 +105,12 @@ public class SysUser implements Serializable {
     @Dict(dictTable ="sys_depart",dicText = "depart_name",dicCode = "org_code")
     private String orgCode;
     //当前登录人单位ID
-   /* @Excel(name="单位",width = 15,dictTable ="sys_depart",dicText = "depart_name",dicCode = "id")*/
+    @Excel(name = "所属村", width = 15)
+    @ApiModelProperty(value = "所属村")
     private String departId;
+    @Excel(name = "所属镇", width = 15)
+    @ApiModelProperty(value = "所属镇")
+    private String zhenId;
     /**部门名称--将不需要序列化的属性前添加关键字transient，序列化对象的时候，这个属性就不会被序列化*/
   /*  @Excel(name = "单位", width = 15)*/
     private transient String orgCodeTxt;
@@ -232,17 +236,32 @@ public class SysUser implements Serializable {
 
     private List<String> roleId;
 
+    /**角色*/
+    @Excel(name = "角色", width = 15)
+    @ApiModelProperty(value = "角色")
     private String role;
+
+    /**身份证号*/
+    @Excel(name = "身份证号", width = 15)
+    @ApiModelProperty(value = "身份证号")
+    private String idnumber;
+
     /**
      * 上次验证手机号码时间
      */
     private Date lastVerifyTime;
 
+    /**户籍编号*/
+    @Excel(name = "户籍编号", width = 15)
+    @ApiModelProperty(value = "户籍编号")
     private String homeCode;
 
-    @ApiModelProperty(value = "户籍关系")
+    @Excel(name = "是否是户主", width = 15)
+    @ApiModelProperty(value = "是否是户主")
     @Dict(dicCode = "home_role")
     private Integer homeRole;
+
+    private Integer relation;
 
     private String officeType;
 }
