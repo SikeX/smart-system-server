abb33f4659c143158cab1f11b69f7d4b2ff71e4c
MrYsL <1501010883@qq.com>
2022-03-05 17:59:29 +0800

    丧事事后报备系统预警

--- smart-system-core/src/main/java/org/jeecg/modules/smartFuneralReport/controller/FuneralSendCaution.java
+++ smart-system-core/src/main/java/org/jeecg/modules/smartFuneralReport/controller/FuneralSendCaution.java
@@ -1,12 +1,12 @@
-package org.jeecg.modules.smartReportingInformation.controller;
+package org.jeecg.modules.smartFuneralReport.controller;
 
 
 import org.jeecg.common.api.dto.message.MessageDTO;
 import org.jeecg.common.system.api.ISysBaseAPI;
+import org.jeecg.modules.smartFuneralReport.entity.SmartFuneralReport;
+import org.jeecg.modules.smartFuneralReport.service.ISmartFuneralReportService;
 import org.jeecg.modules.smartReportingInformation.entity.SmartJob;
-import org.jeecg.modules.smartReportingInformation.entity.SmartReportingInformation;
 import org.jeecg.modules.smartReportingInformation.entity.SysRole;
-import org.jeecg.modules.smartReportingInformation.service.ISmartReportingInformationService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
@@ -16,45 +16,50 @@ import java.util.List;
 
 
 @Component
-public class SendCaution {
+public class FuneralSendCaution {
 
     private static final long ONE_DAY=86400000L;//每天的毫秒数
     @Autowired
-    private ISmartReportingInformationService smartReportingInformationService;
+    private ISmartFuneralReportService smartFuneralReportService;
     @Autowired
     private ISysBaseAPI sysBaseAPI;
 
-    @Scheduled(cron = "0 49 17 * * ?")
+    @Scheduled(cron = "0 8 17 * * ?")
 
     private void sendCaution() {//给管理员发送提醒
         //获取项目的状态（开始或关闭）
         SmartJob jobstatus=
-                smartReportingInformationService.getStatus();
+                smartFuneralReportService.getStatus();
         if(jobstatus.equals("1")){
                 return;
         }
         else{
             //发送消息
-                List<SmartReportingInformation> send =
-                        smartReportingInformationService.sendInformation();
-                for (SmartReportingInformation s : send) {
-                    String status = s.getProcessingResult();
-                    if(status.equals("未受理")){
+                List<SmartFuneralReport> send =
+                        smartFuneralReportService.sendInformation();
+                for (SmartFuneralReport s : send) {
+//                    String status = s.getProcessingResult();
+//                    if(status.equals("未受理")){
                         long now=new Date().getTime();//当前时间
-                        long report = s.getReportingTime().getTime();//举报时间
+                        long report = s.getReportTime().getTime();//口头报备填写时间
                         long time=report-now;
                         time/= ONE_DAY;
-                        if(time>3){
+                        if(time<15){
                             continue;
                         }
                         else{
                             //获取用户角色
-                            List<SysRole> role=smartReportingInformationService.getUser();
+                            List<SysRole> role=smartFuneralReportService.getUser();
+                            String userId = s.getPeopleId();
+                            String peopleName = "人员";
+                            String realname = smartFuneralReportService.getRealnameById(userId);
+                            if(realname != null && !realname.equals(""))
+                            {peopleName = realname;}
                             for(SysRole r:role){
                                 try{
                                     MessageDTO messageDTO=new MessageDTO();
-                                    messageDTO.setTitle("举报信息提醒");
-                                    messageDTO.setContent("您有举报信息还未处理");
+                                    messageDTO.setTitle("丧事事后报备信息提醒");
+                                    messageDTO.setContent(peopleName+"的丧事事后报备已超过15日未提交");
                                     messageDTO.setFromUser("admin");
                                     messageDTO.setToUser(r.getUsername());
                                     messageDTO.setCategory("1");
@@ -65,9 +70,9 @@ public class SendCaution {
 
                                 }
                             }
-                            break;
+//                            break;
                         }
-                    }
+
                 }
             }
     }
---
+++ smart-system-core/src/main/java/org/jeecg/modules/smartFuneralReport/entity/SmartJob.java
@@ -0,0 +1,127 @@
+package org.jeecg.modules.smartFuneralReport.entity;
+
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableId;
+import com.baomidou.mybatisplus.annotation.TableName;
+import com.baomidou.mybatisplus.core.toolkit.IdWorker;
+import com.fasterxml.jackson.annotation.JsonFormat;
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.experimental.Accessors;
+import org.jeecg.common.aspect.annotation.Dict;
+import org.jeecgframework.poi.excel.annotation.Excel;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import java.io.Serializable;
+import java.util.Date;
+
+/**
+ * @Description: TODO
+ * @author: lord
+ * @date: 2021年11月30日 16:02
+ */
+@Data
+@TableName("smart_job")
+@Accessors(chain = true)
+@EqualsAndHashCode(callSuper = false)
+@ApiModel(value="smart_job对象", description="定时任务信息表")
+public class SmartJob implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    /**主键*/
+    @TableId(type = IdType.ASSIGN_ID)
+    @ApiModelProperty(value = "主键")
+    private String id;
+    /**创建人*/
+    @ApiModelProperty(value = "创建人")
+    private String createBy;
+    /**创建日期*/
+    @JsonFormat(timezone = "GMT+8",pattern = "yyyy-MM-dd HH:mm:ss")
+    @DateTimeFormat(pattern="yyyy-MM-dd HH:mm:ss")
+    @ApiModelProperty(value = "创建日期")
+    private Date createTime;
+    /**更新人*/
+    @ApiModelProperty(value = "更新人")
+    private String updateBy;
+    /**更新日期*/
+    @JsonFormat(timezone = "GMT+8",pattern = "yyyy-MM-dd HH:mm:ss")
+    @DateTimeFormat(pattern="yyyy-MM-dd HH:mm:ss")
+    @ApiModelProperty(value = "更新日期")
+    private Date updateTime;
+    /**所属部门*/
+    @ApiModelProperty(value = "所属部门")
+    private String sysOrgCode;
+    /**任务类型*/
+    @Excel(name = "任务类型", width = 15, dicCode = "job_type")
+    @Dict(dicCode = "job_type")
+    @ApiModelProperty(value = "任务类型")
+    private String jobType;
+    /**任务名称*/
+    @Excel(name = "任务名称", width = 15)
+    @ApiModelProperty(value = "任务名称")
+    private String jobName;
+    /**任务描述*/
+    @Excel(name = "任务描述", width = 15)
+    @ApiModelProperty(value = "任务描述")
+    private String jobDescribe;
+    /**任务状态*/
+    @Excel(name = "任务状态", width = 15)
+    @ApiModelProperty(value = "任务状态")
+    private String jobStatus;
+    /**是否每日提醒*/
+    @Excel(name = "是否每日提醒", width = 15, dicCode = "is_loop")
+    @Dict(dicCode = "is_loop")
+    @ApiModelProperty(value = "是否每日提醒")
+    private String isLoop;
+    /**执行时间(日）*/
+    @Excel(name = "执行时间(日）", width = 15, format = "yyyy-MM-dd")
+    @JsonFormat(timezone = "GMT+8",pattern = "yyyy-MM-dd")
+    @DateTimeFormat(pattern="yyyy-MM-dd")
+    @ApiModelProperty(value = "执行时间(日）")
+    private Date executeTimeDay;
+    /**执行时间(时）*/
+    @Excel(name = "执行时间(时）", width = 15)
+    @ApiModelProperty(value = "执行时间(时）")
+    private String executeTimeHour;
+    /**提醒全体人员*/
+    @Excel(name = "提醒全体人员", width = 15, dicCode = "is_loop")
+    @Dict(dicCode = "is_loop")
+    @ApiModelProperty(value = "提醒全体人员")
+    private String isToAll;
+    /**提醒人员*/
+    @Excel(name = "提醒人员", width = 15, dictTable = "sys_user", dicText = "realname", dicCode = "username")
+    @Dict(dictTable = "sys_user", dicText = "realname", dicCode = "username")
+    @ApiModelProperty(value = "提醒人员")
+    private String toUser;
+    /**提醒类型*/
+    @Excel(name = "提醒类型", width = 15, dicCode = "msgType")
+    @Dict(dicCode = "msgType")
+    @ApiModelProperty(value = "提醒类型")
+    private String type;
+    /**模板名*/
+    @Excel(name = "模板名", width = 15)
+    @ApiModelProperty(value = "模板名")
+    private String templateName;
+    /**模板内容*/
+    @Excel(name = "模板内容", width = 15)
+    @ApiModelProperty(value = "模板内容")
+    private String templateContent;
+    /**job bean*/
+    @Excel(name = "job bean", width = 15)
+    @ApiModelProperty(value = "job bean")
+    private String jobBean;
+
+
+    public void test(){
+        executeTimeDay.getTime();
+    }
+
+    public void setJobBean() {
+
+        this.jobBean = IdWorker.getIdStr();
+    }
+
+
+}
