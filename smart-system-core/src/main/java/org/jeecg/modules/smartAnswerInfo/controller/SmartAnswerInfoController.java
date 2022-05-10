c332ee7d1136e3b0b43ade821699f2551ba589d2
xyy <machine.xyy@gmail.com>
2022-05-10 16:31:10 +0800

    feat: 单位考核完成度

--- smart-system-core/src/main/java/org/jeecg/modules/smartAnswerInfo/controller/SmartAnswerInfoController.java
+++ smart-system-core/src/main/java/org/jeecg/modules/smartAnswerInfo/controller/SmartAnswerInfoController.java
@@ -27,7 +27,9 @@ import org.jeecg.modules.smartAssessmentContent.service.ISmartAssessmentContentS
 import org.jeecg.modules.smartAssessmentDepartment.entity.SmartAssessmentDepartment;
 import org.jeecg.modules.smartAssessmentDepartment.service.ISmartAssessmentDepartmentService;
 import org.jeecg.modules.smartAssessmentMission.entity.SmartAssessmentDepart;
+import org.jeecg.modules.smartAssessmentMission.entity.SmartAssessmentMission;
 import org.jeecg.modules.smartAssessmentMission.service.ISmartAssessmentDepartService;
+import org.jeecg.modules.smartAssessmentMission.service.ISmartAssessmentMissionService;
 import org.jeecg.modules.smartAssessmentTeam.entity.SmartAssessmentTeam;
 import org.jeecg.modules.smartAssessmentTeam.service.ISmartAssessmentTeamService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -56,6 +58,9 @@ public class SmartAnswerInfoController extends JeecgController<SmartAnswerInfo, 
 
     @Autowired
     private ISmartAnswerAssContentService smartAnswerAssContentService;
+
+    @Autowired
+    private ISmartAssessmentMissionService smartAssessmentMissionService;
 
     @Autowired
     private ISmartAssessmentContentService smartAssessmentContentService;
@@ -260,6 +265,38 @@ public class SmartAnswerInfoController extends JeecgController<SmartAnswerInfo, 
     public Result<?> edit(@RequestBody SmartAnswerInfo smartAnswerInfo) {
         smartAnswerInfoService.updateById(smartAnswerInfo);
         return Result.OK("编辑成功!");
+    }
+
+    /**
+     * 更新完成度
+     *
+     * @param smartAnswerInfo
+     * @return
+     */
+    @AutoLog(value = "答题信息表-更新完成度")
+    @ApiOperation(value = "答题信息表-更新完成度", notes = "答题信息表-更新完成度")
+    @PutMapping(value = "/updateCompletionDegree")
+    public Result<?> updateCompletionDegree(@RequestBody SmartAnswerInfo smartAnswerInfo) {
+        // 查询上传文件数目大于0的要点个数
+        QueryWrapper<SmartAnswerAssContent> assContentQueryWrapper = new QueryWrapper<>();
+        assContentQueryWrapper.eq("main_id", smartAnswerInfo.getId())
+                .eq("is_key", 1)
+                .eq("has_child", "0")
+                .ne("upload_count", 0);
+        double count = smartAnswerAssContentService.count(assContentQueryWrapper);
+        smartAnswerInfo.setFinishedPoint((int) count);
+        SmartAssessmentMission mission = smartAssessmentMissionService.getById(smartAnswerInfo.getMissionId());
+        if (oConvertUtils.isEmpty(mission)) {
+            return Result.error("考核任务出错了！");
+        }
+
+        if (mission.getKeyPointsAmount() != 0) {
+            smartAnswerInfo.setCompletionDegree(count / mission.getKeyPointsAmount());
+        }
+
+
+        smartAnswerInfoService.updateById(smartAnswerInfo);
+        return Result.OK("更新完成度成功!");
     }
 
     /**
