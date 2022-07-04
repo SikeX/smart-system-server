f3f01ef296052bfef2e20fe7fd5c1b6115eef4c6
Purpose <machine.xyy@gmail.com>
2022-07-04 21:20:41 +0800

    fix: 最终评分上传内容分页结果 (#195)

--- smart-system-core/src/main/java/org/jeecg/modules/smartAnswerInfo/service/ISmartAnswerInfoService.java
+++ smart-system-core/src/main/java/org/jeecg/modules/smartAnswerInfo/service/ISmartAnswerInfoService.java
@@ -1,5 +1,6 @@
 package org.jeecg.modules.smartAnswerInfo.service;
 
+import com.baomidou.mybatisplus.core.metadata.IPage;
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import org.apache.ibatis.annotations.Param;
 import org.jeecg.modules.smartAnswerInfo.entity.SmartAnswerInfo;
@@ -15,7 +16,7 @@ import java.util.List;
  * @Version: V1.0
  */
 public interface ISmartAnswerInfoService extends IService<SmartAnswerInfo> {
-    List<SmartDepartContentScore> selectByMissionIdAndContentId(Page<SmartDepartContentScore> page, @Param("missionId") String missionId, @Param("assContentId") String assContentId);
+    IPage<SmartDepartContentScore> selectByMissionIdAndContentId(Page<SmartDepartContentScore> page, @Param("missionId") String missionId, @Param("assContentId") String assContentId);
 
     /**
      * 更新答题信息表中的任务状态
